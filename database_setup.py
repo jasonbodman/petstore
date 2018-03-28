cac1c4c503aecaf1d5cd049bca37e8a4848b725c
Jason Bodman <bodmand1@gmail.com>
2018-03-27 21:01:50 -0400

    Create database_setup.py file

---
+++ database_setup.py
@@ -0,0 +1,36 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.delcarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Type(Base):
+    __tablename__ = "type"
+
+    name = Column(String(32), nullable = False)
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+class Pet(Base):
+    __tablename__ = 'pet'
+
+    name = Column(String(32), nullable = False)
+    id = Column(Integer, primary_key = True)
+    type = Column(Integer, ForeignKey('type.id'))
+    description = Column(String)
+    user = Column(Integer, ForeignKey('user.id'))
+    adopted = Column(Integer)
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key = True)
+    username = Column(String(32), index = True)
+    picture = Column(String)
+    email = Column(String)
+
+engine = create_engine('sqlite:///itemcatalog.db')
+Base.metadata.create_all(engine)
