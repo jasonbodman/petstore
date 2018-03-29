e965b3ddbc717a51c44f2cb3c45181598597028f
Jason Bodman <bodmand1@gmail.com>
2018-03-29 21:22:20 +0000

    update ForeignKey columns to be objects instead of strings

--- lotsofpets.py
+++ lotsofpets.py
@@ -15,24 +15,24 @@ User1 = User(username="Jason Bodman", email="bodmand1@gmail.com", picture="https
 session.add(User1)
 session.commit()
 
-Type1 = Type(name="Cat", user_id="User1")
+Type1 = Type(name="Cat", user_id=User1)
 session.add(Type1)
 session.commit()
 
-Type2 = Type(name="Dog", user_id="User1")
+Type2 = Type(name="Dog", user_id=User1)
 session.add(Type2)
 session.commit()
 
-Pet1 = Pet(name="Ginny", type="Type1", description="Ginny is fun but lazy cat who isn't shy to let you know when it is meal time.", user="User1", adopted="1")
+#Create pets
+Pet1 = Pet(name="Ginny", type=Type1, description="Ginny is fun but lazy cat who isn't shy to let you know when it is meal time.", user="User1", adopted="1")
 session.add(Pet1)
 session.commit()
 
-
-Pet2 = Pet(name="Penny", type="Type1", description="Penny is an adventerous cat who likes naps and tends to be shy.", user="User1", adopted="1")
+Pet2 = Pet(name="Penny", type=Type1, description="Penny is an adventerous cat who likes naps and tends to be shy.", user="User1", adopted="1")
 session.add(Pet2)
 session.commit()
 
-Pet3 = Pet(name="Murphy", type="Type2", description="If you're looking for a furry friend who is ready for adventure and looking to make you happy, you've found him in Murphy!", user="User1", adopted="1")
+Pet3 = Pet(name="Murphy", type=Type2, description="If you're looking for a furry friend who is ready for adventure and looking to make you happy, you've found him in Murphy!", user="User1", adopted="1")
 session.add(Pet3)
 session.commit()
 
