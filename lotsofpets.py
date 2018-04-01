94a65ab6415162b9d982e4726a3b0a7a92f28434
Jason Bodman <bodmand1@gmail.com>
2018-04-01 03:56:21 +0000

    remove adopted Column from pre-populated list

--- lotsofpets.py
+++ lotsofpets.py
@@ -25,15 +25,15 @@ session.add(Type2)
 session.commit()
 
 # Create pets
-Pet1 = Pet(name="Ginny", type=Type1.id, description="Ginny is fun but lazy cat who isn't shy to let you know when it is meal time.", user=User1.id, adopted="1")
+Pet1 = Pet(name="Ginny", type=Type1.id, description="Ginny is fun but lazy cat who isn't shy to let you know when it is meal time.", user=User1.id)
 session.add(Pet1)
 session.commit()
 
-Pet2 = Pet(name="Penny", type=Type1.id, description="Penny is an adventerous cat who likes naps and tends to be shy.", user=User1.id, adopted="1")
+Pet2 = Pet(name="Penny", type=Type1.id, description="Penny is an adventerous cat who likes naps and tends to be shy.", user=User1.id)
 session.add(Pet2)
 session.commit()
 
-Pet3 = Pet(name="Murphy", type=Type2.id, description="If you're looking for a furry friend who is ready for adventure and looking to make you happy, you've found him in Murphy!", user=User1.id, adopted="1")
+Pet3 = Pet(name="Murphy", type=Type2.id, description="If you're looking for a furry friend who is ready for adventure and looking to make you happy, you've found him in Murphy!", user=User1.id)
 session.add(Pet3)
 session.commit()
 
